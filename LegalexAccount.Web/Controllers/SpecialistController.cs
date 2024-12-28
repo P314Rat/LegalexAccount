e44cf12bb07a6cda8806cd5a183b575084a491e8
Tsimafei Lipnitski <silencetray@gmail.com>
2024-12-28 15:54:39 +0300

    Start to create "Add case" form.

---
+++ LegalexAccount.Web/Controllers/SpecialistController.cs
@@ -0,0 +1,18 @@
+﻿using LegalexAccount.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LegalexAccount.Web.Controllers
+{
+    public class SpecialistController : BaseController
+    {
+        public SpecialistController(IApplicationDbContextFactory _dbContextFactory, IHttpContextAccessor httpContextAccessor) : base(_dbContextFactory, httpContextAccessor)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
